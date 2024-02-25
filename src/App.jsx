fbc642d197e87d90f37b389865cbf7076343b6e7
Bao Than <109579681+BaoThan@users.noreply.github.com>
2024-02-24 22:24:19 -0800

    Add time out feature & run lint (#1)
    Co-authored-by: Bao Than <b.than@sfsu.edu>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import './App.css';
-import './components/Resizer.css';
+import React from "react";
+import "./App.css";
+import "./components/Resizer.css";
 
-import SplitPane from 'react-split-pane';
+import SplitPane from "react-split-pane";
 
 // import AceEditor from 'react-ace';
-import Header from './components/Header';
-import OutputPanel from './components/OutputPanel';
-import { CodeExecutionResult, executeCode } from './services/codeExecution';
+import Header from "./components/Header";
+import OutputPanel from "./components/OutputPanel";
+import { executeCode } from "./services/codeExecution";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 // import ace editor features
-import 'ace-builds/src-noconflict/ace';
-import 'ace-builds/src-noconflict/ext-language_tools';
-import 'ace-builds/src-noconflict/ext-searchbox';
+import "ace-builds/src-noconflict/ace";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/ext-searchbox";
 import "react-ace-builds/webpack-resolver-min";
 import AceEditor from "react-ace-builds";
 
@@ -39,124 +44,136 @@ import "ace-builds/src-noconflict/theme-terminal";
 import "ace-builds/src-noconflict/theme-solarized_dark";
 
 const themeOptions = [
-  { value: 'ambiance', label: 'Ambiance' },
-  { value: 'chaos', label: 'Chaos' },
-  { value: 'chrome', label: 'Chrome' },
-  { value: 'clouds', label: 'Clouds' },
-  { value: 'clouds_midnight', label: 'Clouds Midnight' },
-  { value: 'dracula', label: 'Dracula' },
-  { value: 'eclipse', label: 'Eclipse' },
-  { value: 'github', label: 'Github' },
-  { value: 'monokai', label: 'Monokai' },
-  { value: 'one_dark', label: 'One Dark' },
-  { value: 'terminal', label: 'Terminal' },
-  { value: 'solarized_dark', label: 'Solarized Dark' }
+  { value: "ambiance", label: "Ambiance" },
+  { value: "chaos", label: "Chaos" },
+  { value: "chrome", label: "Chrome" },
+  { value: "clouds", label: "Clouds" },
+  { value: "clouds_midnight", label: "Clouds Midnight" },
+  { value: "dracula", label: "Dracula" },
+  { value: "eclipse", label: "Eclipse" },
+  { value: "github", label: "Github" },
+  { value: "monokai", label: "Monokai" },
+  { value: "one_dark", label: "One Dark" },
+  { value: "terminal", label: "Terminal" },
+  { value: "solarized_dark", label: "Solarized Dark" },
 ];
 
 const languageOptions = [
-  { value: 'c', label: 'C' },
-  { value: 'csharp', label: 'C#' },
-  { value: 'cpp', label: 'C++' },
-  { value: 'java', label: 'Java' },
-  { value: 'javascript', label: 'Javascript' },
-  { value: 'python', label: 'Python' },
-  { value: 'ruby', label: 'Ruby' }
+  { value: "c", label: "C" },
+  { value: "csharp", label: "C#" },
+  { value: "cpp", label: "C++" },
+  { value: "java", label: "Java" },
+  { value: "javascript", label: "Javascript" },
+  { value: "python", label: "Python" },
+  { value: "ruby", label: "Ruby" },
 ];
 
 const languageValueToAceLanguage = {
-  c: 'c_cpp',
-  csharp: 'csharp',
-  cpp: 'c_cpp',
-  java: 'java',
-  javascript: 'javascript',
-  python: 'python',
-  ruby: 'ruby'
-}
-
+  c: "c_cpp",
+  csharp: "csharp",
+  cpp: "c_cpp",
+  java: "java",
+  javascript: "javascript",
+  python: "python",
+  ruby: "ruby",
+};
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      language: 'c',
-      aceLanguage: 'c_cpp',
-      theme: 'ambiance',
-      code: '',
-      output: ''
+      language: "c",
+      aceLanguage: "c_cpp",
+      theme: "ambiance",
+      code: "",
+      output: "",
     };
   }
 
   onLanguageChanged = (language) => {
-    this.setState(
-      { language: language, aceLanguage: languageValueToAceLanguage[language] }
-    );
-  }
+    this.setState({
+      language: language,
+      aceLanguage: languageValueToAceLanguage[language],
+    });
+  };
 
   onThemeChanged = (theme) => {
     this.setState({ theme: theme });
-  }
+  };
 
   onEdit = (newValue) => {
     this.setState({ code: newValue });
-  }
+  };
 
   onRunButtonClicked = async () => {
     const result = await executeCode(this.state.language, this.state.code);
     if (result == null) {
-      console.log('something went wrong');
+      console.log("something went wrong");
       return;
     }
-
-    const outputStr = (result.exitCode == 0) ? result.stdout : result.stderr;
-    this.setState({ output: outputStr});
-  }
+    if (result.timeout) {
+      NotificationManager.error("Execution Time Out");
+      return;
+    }
+    const outputStr = result.exitCode === 0 ? result.stdout : result.stderr;
+    this.setState({ output: outputStr });
+  };
 
   render() {
     const paneChildStyle = {
-      height: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
     };
     return (
       <div id="App">
         {/*Header bar on top of the screen*/}
-        <Header themeOptions={themeOptions}
-                languageOptions={languageOptions}
-                onLanguageChanged={this.onLanguageChanged}
-                onThemeChanged={this.onThemeChanged}
-                onRunButtonClicked={this.onRunButtonClicked}/>
+        <Header
+          themeOptions={themeOptions}
+          languageOptions={languageOptions}
+          onLanguageChanged={this.onLanguageChanged}
+          onThemeChanged={this.onThemeChanged}
+          onRunButtonClicked={this.onRunButtonClicked}
+        />
 
         {/*Containing the code editor and the output panel*/}
-        <SplitPane id="workspace"
-                   split='vertical'
-                   minSize={200}
-                   defaultSize={'70vw'} >
+        <SplitPane
+          id="workspace"
+          split="vertical"
+          minSize={200}
+          defaultSize={"70vw"}
+        >
           <div style={paneChildStyle}>
-            <AceEditor  mode={this.state.aceLanguage}
-                        theme={this.state.theme}
-                        value={this.state.code}
-                        onChange={this.onEdit}
-                        width='100%'
-                        height='100%'
-                        fontSize={16}
-                        setOptions={{
-                          enableBasicAutocompletion: true,
-                          enableLiveAutocompletion: true,
-                          enableSnippets: true,
-                          showLineNumbers: true,
-                          showGutter: true
-                        }}/>
+            <AceEditor
+              mode={this.state.aceLanguage}
+              theme={this.state.theme}
+              value={this.state.code}
+              onChange={this.onEdit}
+              width="100%"
+              height="100%"
+              fontSize={16}
+              setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: true,
+                showLineNumbers: true,
+                showGutter: true,
+              }}
+            />
           </div>
-          <div style={{...paneChildStyle}}>
-            <OutputPanel  id="outputPanel"
-                          theme={this.state.theme}
-                          height='100%'
-                          width='100%'
-                          value={this.state.output} />
+          <div style={{ ...paneChildStyle }}>
+            <OutputPanel
+              id="outputPanel"
+              theme={this.state.theme}
+              height="100%"
+              width="100%"
+              value={this.state.output}
+            />
           </div>
         </SplitPane>
 
+        <NotificationContainer />
       </div>
     );
   }
