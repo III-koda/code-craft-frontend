ed7f982fb4a06b727fe7067d4c3625a1e405bcc4
Bao Than <b.than@sfsu.edu>
2024-02-24 22:19:14 -0800

    Add time out feature

--- src/services/codeExecution.js
+++ src/services/codeExecution.js
@@ -1,48 +1,63 @@
-import axois from 'axios';
+import axois from "axios";
 
-const CODE_EXE_API_URL = 'https://code-craft-grso.onrender.com/execute'
+const CODE_EXE_API_URL = "https://code-craft-grso.onrender.com/execute";
 
 class CodeExecutionResult {
-    constructor(stdout, stderr, exitCode) {
-        this.#stdout = stdout;
-        this.#stderr = stderr;
-        this.#exitCode = exitCode;
-    }
+  constructor(stdout, stderr, exitCode, timeout) {
+    this.#stdout = stdout;
+    this.#stderr = stderr;
+    this.#exitCode = exitCode;
+    this.#timeout = timeout;
+  }
 
-    get stdout() { return this.#stdout; }
-    get stderr() { return this.#stderr; }
-    get exitCode() { return this.#exitCode; }
+  get stdout() {
+    return this.#stdout;
+  }
+  get stderr() {
+    return this.#stderr;
+  }
+  get exitCode() {
+    return this.#exitCode;
+  }
+  get timeout() {
+    return this.#timeout;
+  }
 
-    #stdout
-    #stderr
-    #exitCode
+  #stdout;
+  #stderr;
+  #exitCode;
+  #timeout;
 }
 
 const executeCode = async (language, code) => {
-    try {
-        const conf = {
-            headers: { 'Content-Type': 'multipart/form-data' }
-        };
-        const formData = new FormData();
-        formData.append('language', language);
-        formData.append('code', code);
+  try {
+    const conf = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+    const formData = new FormData();
+    formData.append("language", language);
+    formData.append("code", code);
 
-        const response = await axois.post(CODE_EXE_API_URL, formData, conf);
-        if (response.status != 200 || !response.data.success) {
-            console.log(response)
-            return null;
-        }
-        const data = response.data.data;
-        const result = new CodeExecutionResult(
-            data.stdout,
-            data.stderr,
-            data.exit_code
-        );
-        return result;
-    } catch (error) {
-        console.log(`ERRORL: ${error}`)
-        return null;
+    const response = await axois.post(CODE_EXE_API_URL, formData, conf);
+    if (response.status !== 200) {
+      console.log(response);
+      return null;
     }
+    const data = response.data.data;
+    const timeout = response.data.time_out;
+    if (timeout) {
+      return new CodeExecutionResult(null, null, null, true);
+    }
+    return new CodeExecutionResult(
+      data.stdout,
+      data.stderr,
+      data.exit_code,
+      data.timeout
+    );
+  } catch (error) {
+    console.log(`ERRORL: ${error}`);
+    return null;
+  }
 };
 
 export { CodeExecutionResult, executeCode };
