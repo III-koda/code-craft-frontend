e0aa891866703d6b16fdc9744f0595ebc17ab27e
Tri Huynh <trihuynh0221@gmail.com>
2024-03-23 00:01:03 -0700

    Add support for PHP and Perl

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,11 @@ import 'ace-builds/src-noconflict/mode-c_cpp'
 import 'ace-builds/src-noconflict/mode-csharp'
 import 'ace-builds/src-noconflict/mode-java'
 import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/mode-perl'
+import 'ace-builds/src-noconflict/mode-php'
 import 'ace-builds/src-noconflict/mode-python'
 import 'ace-builds/src-noconflict/mode-ruby'
+
 
 // import the themes
 import 'ace-builds/src-noconflict/theme-ambiance'
@@ -64,6 +67,8 @@ const languageOptions = [
   { value: 'cpp', label: 'C++' },
   { value: 'java', label: 'Java' },
   { value: 'javascript', label: 'Javascript' },
+  { value: 'perl', label: 'Perl' },
+  { value: 'php', label: 'Php' },
   { value: 'python', label: 'Python' },
   { value: 'ruby', label: 'Ruby' }
 ]
@@ -74,6 +79,8 @@ const languageValueToAceLanguage = {
   cpp: 'c_cpp',
   java: 'java',
   javascript: 'javascript',
+  perl: 'perl',
+  php: 'php',
   python: 'python',
   ruby: 'ruby'
 }
